10e90f316a8c2a029e84e549e5a8c14980d7623d
ThreeeJ <166373147+ThreeeJ@users.noreply.github.com>
2024-05-09 18:38:29 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int grade;
+	cin >> grade;
+	
+	if (grade >= 90) cout << "A" << endl;
+	else if (grade >= 80) cout << "B" << endl;
+	else if (grade >= 70) cout << "C" << endl;
+	else if (grade >= 60) cout << "D" << endl;
+	else cout << "F" << endl;
+}
