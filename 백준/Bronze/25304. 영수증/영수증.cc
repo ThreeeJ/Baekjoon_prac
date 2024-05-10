e2526ade1b17fbed4cce77fe4878dc8f5c3b4f98
ThreeeJ <166373147+ThreeeJ@users.noreply.github.com>
2024-05-10 14:36:24 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int X, N, total = 0;
+	cin >> X >> N;
+
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		total += a * b;
+	}
+	
+	if (X == total) cout << "Yes" << endl;
+	else cout << "No" << endl;
+}
