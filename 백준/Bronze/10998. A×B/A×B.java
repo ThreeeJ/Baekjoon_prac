0dc44dd6620150ca88b9c2bc656e49eeaa41331e
ThreeeJ <166373147+ThreeeJ@users.noreply.github.com>
2024-08-11 04:33:21 +0900

    [Bronze V] Title: A×B, Time: 164 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args ) {
+		Scanner s = new Scanner(System.in); // scanner 객체 열기
+		
+		int A, B;
+		A = s.nextInt();
+		B = s.nextInt();
+		
+		System.out.println(A * B);
+		
+		s.close(); // scanner 객체 닫기
+	}
+}
