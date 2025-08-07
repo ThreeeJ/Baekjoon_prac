1f23780f30cbd1ca2f9ff944838361117621bdaf
JongJin <166373147+ThreeeJ@users.noreply.github.com>
2025-08-07 23:25:20 +0900

    [Bronze V] Title: WARBOY, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int Wprice = B / A * 3 * C;
+
+        bw.write(String.valueOf(Wprice));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
