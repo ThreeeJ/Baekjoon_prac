9ba1493b3e07de4d189e1b1471037423f4edf47c
JongJin <166373147+ThreeeJ@users.noreply.github.com>
2025-08-29 21:01:07 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 244 ms, Memory: 20384 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+        String num = br.readLine();
+		long remainder = 0;
+		for(int i=0; i < num.length(); i++) {
+			remainder = (remainder * 10 + (num.charAt(i) - '0')) % 20000303;
+		}
+		bw.append(remainder + "\n");
+		
+        bw.flush();
+		
+        bw.close();
+		br.close();
+	}
+}
