34128f19b9f73260277aab295e87c8c45a9e9144
JongJin <166373147+ThreeeJ@users.noreply.github.com>
2025-09-04 23:31:02 +0900

    [Bronze V] Title: Plane, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int k1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int k2 = Integer.parseInt(st.nextToken());
+
+        int total = n1 * k1 + n2 * k2;
+        bw.write(String.valueOf(total));
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
